699dc07d453e9cd8315ae393eee89c031bf8120c
felix <felixeisenmenger@gmx.net>
2023-07-16 11:56:16 +0200

    chore: join *.hpp data into *.cpp file as pybind can't resolve it

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 import pathlib
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
-from setuptools import find_packages, setup
+from setuptools import setup
 
-__version__ = "0.0.30"
+__version__ = "0.0.31"
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
@@ -17,14 +17,6 @@ README = (HERE / "README.md").read_text()
 # Note:
 #   Sort input source files if you glob sources to ensure bit-for-bit
 #   reproducible builds (https://github.com/pybind/python_example/pull/53)
-
-
-class CustomBuildExt(build_ext):
-    def run(self):
-        # Include additional header file directories
-        self.include_dirs.append("src/docstringFormat.hpp")
-        super().run()
-
 
 ext_modules = [
     Pybind11Extension(
@@ -57,7 +49,7 @@ setup(
     extras_require={"test": "pytest"},
     # Currently, build_ext only provides an optional "highest supported C++
     # level" feature, but in the future it may provide more features.
-    cmdclass={"build_ext": CustomBuildExt},
+    cmdclass={"build_ext": build_ext},
     zip_safe=False,
     python_requires="<3.12",
 )
